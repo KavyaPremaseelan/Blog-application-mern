f528e153f0fb010507b431b5954777e9c8a2c1f3
KavyaPremaseelan <kavyapremaseelan@gmail.com>
2024-04-14 11:05:30 +0530

    create the header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,15 +5,19 @@ import Dashboard from './pages/Dashboard'
 import Home from './pages/Home'
 import SignUp from './pages/SignUp'
 import SignIn from './pages/Signin'
+import { Projects } from './pages/Projects'
+import Header from './components/Header'
 
 
 const App = () => {
   return (
     <BrowserRouter>
+    <Header/>
     <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/about' element={<About/>}/>
       <Route path='/dashboard' element={<Dashboard/>}/>
+      <Route path='/projects' element={<Projects/>}/>
       <Route path='/signin' element={<SignIn/>}/>
       <Route path='/signup' element={<SignUp/>}/>
     </Routes>
