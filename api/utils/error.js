01c0d164af344f613cffcf4ef76bbe1e19665a54
KavyaPremaseelan <kavyapremaseelan@gmail.com>
2024-04-14 21:51:52 +0530

    create signup ui

--- api/utils/error.js
+++ api/utils/error.js
@@ -1,8 +1,6 @@
-import { error } from "console"
-
 export const errorHandler = (statusCode, message)=>
 {
-    const err = new Error()
+    const error = new Error()
     error.statusCode = statusCode
     error.message = message
     return error
